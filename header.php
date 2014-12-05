90444d61f19d783f2db345e63951513d0589c08e
KSAS Communications <ksasweb@jhu.edu>
2014-12-05 16:30:56 -0500

    Modified Header for mobile and desktop display
    added screenshot

--- header.php
+++ header.php
@@ -38,15 +38,18 @@
 <?php $theme_option = flagship_sub_get_global_options(); $color_scheme = $theme_option['flagship_sub_color_scheme']; global $blog_id; $site_id = 'site-' . $blog_id; ?>
 <body <?php body_class($color_scheme . ' ' . $site_id); ?>>	
 	<header>
-		<div class="row hide-for-large-up">
+	   <div id="mobile-nav">
+		<div class="row">
 			<div class="small-12 large-4 columns centered blue_bg">
 			<div class="mobile-logo centered"><a href="<?php echo network_site_url(); ?>">Home</a></div>
 			<h2 class="white" align="center"><?php echo get_bloginfo( 'title' ); ?></h2>
 			</div>
 		</div>
-	
+		</div>
+		
+	   <div id="desktop-nav">
 		<div class="row hide-for-print">
-			<div id="search-bar" class="small-12 large-5 large-offset-7 columns">
+			<div id="search-bar" class="small-12 medium-5 medium-offset-7 columns">
 				<div class="row">
 					<div class="small-6 columns">
 					<?php $theme_option = flagship_sub_get_global_options(); 
@@ -65,21 +68,21 @@
 							'fallback_cb' => 'foundation_page_menu', 
 							'container' => 'div',
 							'container_id' => 'search_links', 
-							'container_class' => 'small-6 columns links inline hide-for-small-only',
+							'container_class' => 'small-6 columns links inline mobile-two',
 							'depth' => 1,
 							'items_wrap' => '%3$s', )); ?> 
 				</div>	
 			</div>	<!-- End #search-bar	 -->
 		</div>
-		<div class="row hide-for-medium-up">
-			<div class="medium-12 columns hide-for-small-only" id="logo_nav">
+		<div class="row" id="department">
+			<div class="medium-12 columns" id="logo_nav">
 				<li class="logo"><a href="<?php echo network_home_url(); ?>" title="Krieger School of Arts & Sciences">Krieger School of Arts & Sciences</a></li>
 				
 				<a href="<?php echo site_url(); ?>"><h1 class="white"><span class="small"><?php echo get_bloginfo ( 'description' ); ?></span>					<?php echo get_bloginfo( 'title' ); ?></h1></a>
 			
 			</div>
 		</div>
-		<div class="row hide-for-medium-up hide-for-print">
+		<div class="row hide-for-print">
 			<?php wp_nav_menu( array( 
 				'theme_location' => 'main_nav', 
 				'menu_class' => 'nav-bar', 
@@ -89,5 +92,6 @@
 				'fallback_cb' => 'foundation_page_menu',
 				'walker' => new foundation_navigation(),
 				'depth' => 2  )); ?> 
+		</div>
 		</div>
 		</header>
