ae62c40c25ff12166a58d4dbeeffd96f291b3b06
ksascomm <ksasweb@jhu.edu>
2016-10-24 14:16:38 -0400

    created minified style.css

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 	<![endif]-->
 
 	<!-- CSS Files: Conditionals -->
-	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/style.css">  
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/style.min.css">  
 
   <?php wp_head(); ?>
 
