c52aba9cd5bf3a84c54a11a5a9ee57fbc00dee21
ksascomm <ksasweb@jhu.edu>
2017-10-17 11:06:12 -0400

    add Open Graph tags

--- header.php
+++ header.php
@@ -10,6 +10,15 @@
 	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri() ?>/assets/images/apple-touch-icon-114x114-precomposed.png" />
 	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri() ?>/assets/images/apple-touch-icon-72x72-precomposed.png" />
 	<link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri() ?>/assets/images/apple-touch-icon-57x57-precomposed.png" />
+
+	<meta property="og:site_name" content="Krieger School of Arts and Sciences" />
+    <meta property="og:title" content="<?php echo get_bloginfo( 'title' ); ?> | Krieger School of Arts and Sciences | Johns Hopkins University" />
+    <meta property="og:url" content="<?php echo site_url(); ?>" />
+    <meta property="og:description" content="The Zanvyl Krieger School of Arts and Sciences is the core institution of Johns Hopkins University’s Homewood campus" />
+
+    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/assets/images/gilman.jpg" />
+	<meta property="og:image:width" content="1600" />
+	<meta property="og:image:height" content="400" />
   
 	<!-- CSS Files: All pages -->
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/stylesheets/app.min.css">
